2e8525cc71fa641d769935947cb4df77281d5a9b
Alex Miles Younger <alex@alexmilesyounger.com>
2016-02-07 10:09:16 -0500

    Add basic save function for form data

--- app.py
+++ app.py
@@ -7,4 +7,12 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+
+# create a new route with a function for 
+# data submitted by the form
+# only accessible if you POST to it
+@app.route('/save', methods=['POST'])
+def save():
+	return 'Saved!'
+
 app.run(debug=True)
