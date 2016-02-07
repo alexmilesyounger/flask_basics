c5b6c9e8489317c894dfaaafde421929b296fb13
Alex Miles Younger <alex@alexmilesyounger.com>
2016-02-07 10:18:14 -0500

    Documentation

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# This next video is about how to remember user info using cookies
+
 from flask import Flask
 from flask import redirect
 from flask import render_template
@@ -10,20 +12,8 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-
-# create a new route with a function for 
-# data submitted by the form
-# only accessible if you POST to it
 @app.route('/save', methods=['POST'])
 def save():
-	import pdb; pdb.set_trace() # python debugger
-	# with pdb save the form and look at the console
-	# I'll see what's going on and the (Pdb) prompt will 
-	# show up. From here I can look around inside the 
-	# script while it's running. One of the big things
-	# to look at here is Flask's request module.
-	# We took a look at request.form
-	# Spending some time with request would be useful.
 	return redirect(url_for('index'))
 
 app.run(debug=True)
