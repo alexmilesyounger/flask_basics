0882f53603d4f1ea8f8230dcea0c87cb59d74e9b
Alex Miles Younger <alex@alexmilesyounger.com>
2016-02-06 21:08:48 -0500

    Add setup for heroku

--- simple_app.py
+++ simple_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import twilio.twiml
 
 app = Flask(__name__)
 
@@ -18,6 +19,16 @@ def index(name="Treehouse"): # FUNCTION / VIEW
 def add(num1, num2):
 	context = {'num1': num1, 'num2': num2}
 	return render_template("add.html", **context)
+
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def reply_sms(reply_message="wtf?"):
+	"""Respond to player with sms message."""
+	resp = twilio.twiml.Response()
+	resp.sms(reply_message)
+	return str(resp)
+
 
 # RUN
 app.run(debug=True)
