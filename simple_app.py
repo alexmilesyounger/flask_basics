ee31d5831cbe0791447b9570f592914d3cc630c7
Alex Miles Younger <alex@alexmilesyounger.com>
2016-02-06 19:12:04 -0500

    Accepting numbers converting to integers and adding

--- simple_app.py
+++ simple_app.py
@@ -1,14 +1,16 @@
 from flask import Flask
-from flask import request
 
 app = Flask(__name__)
 
 @app.route('/') # ROUTE
 @app.route('/<name>') # ALTERNATE ROUTE
 def index(name="Treehouse"): # FUNCTION / VIEW
-	name = request.args.get('name', name)
 	return "Hello from {}".format(name)
 
+
+@app.route('/add/<int:num1>/<int:num2>') # Add two numbers
+def add(num1, num2):
+	return '{} + {} = {}'.format(num1, num2, num1 + num2) # return string
 
 # RUN
 app.run(debug=True)
